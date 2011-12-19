583e9c81f953b0e225db4de8e4039a8cc9d3304a
Kleber Hamada Sato <kleberhs007@yahoo.com>
2011-12-18 23:33:41 -0200

    Commit inicial

---
+++ par_impar.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @author Kleber Hamada Sato
+ * @copyright 2011
+ */
+ 
+ //essa função verifica se o número é par ou ímpar
+ 
+function numPar($numero)
+{
+   echo ($numero%2==0)? 'É par':'É ímpar';
+    
+}
+
+//captura do input mediante o método POST do formulário da página principal (index)
+
+$valor = $_POST['numero'];
+
+// executa a função numPar e atribui o valor a uma variável
+$numero = numPar($valor);
+
+//imprime a variável $numero
+echo $numero;
+
+
+?>
+
+<html>
+<body>
+<p>
+<a href="index.php">Voltar</a>
+</p>
+</body>
+</html>
