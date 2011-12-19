c956173b0557ce6661d558d0e11ea9e31b586256
Kleber Hamada Sato <kleberhs007@yahoo.com>
2011-12-19 14:59:26 -0200

    Insere validacao de dados (o parametro a ser passado no formulario deve ser um numero).
    Signed-off-by: Kleber Hamada Sato <kleberhs007@yahoo.com>

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<!-- formulário HTML que recebe o parâmetro do número -->
+
+<head>
+<title>Numero par ou impar</title>
+
+</head>
+
+<body>
+<form action="par_impar.php" method="post">
+
+<input type="text" name="numero" id="numero" />
+<input type="submit" value="Verificar" />
+
+</form>
+
+</body>
+
+</html>
